2e55218a9751aadd0ec38a106d039965414fe948
tony <anton@azid.ru>
2017-04-01 02:03:51 +0300

    fix names

--- conf.js
+++ conf.js
@@ -11,15 +11,15 @@ exports.storage = 'sqlite';
 
 
 exports.hub = 'byteball.org/bb';
-exports.deviceName = 'Headless';
+exports.deviceName = 'Price Oracle';
 exports.permanent_paring_secret = 'randomstring';
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
 exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 //Wallet uses first address if bSingleAddress == true or you have to tell it what address to use with dataFeedAddress parameter
 exports.bSingleAddress = true;
 exports.dataFeedAddress = "";
-exports.minAvailableDataFeedings = 2;
+exports.minAvailableDataFeedings = 100;
 
 exports.KEYS_FILENAME = 'keys.json';
 
-console.log('finished headless conf');
+console.log('finished price oracle conf');
