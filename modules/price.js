3d8871fc2457b0f6a7a4f85c31e1f81a45d17a78
Tarmo <tarmo888@hotmail.com>
2020-03-05 17:51:52 +0200

    rename

--- modules/price.js
+++ modules/price.js
@@ -6,21 +6,21 @@ function formatPriceToPrecision(fPrice, accuracy = 6) {
 		throw Error("1 for accuracy is min");
 	if (accuracy > 16)
 		throw Error("16 for accuracy is max");
-	let price_in_usd = fPrice.toFixed(18);
-	let arr = price_in_usd.split('.');
+	let price_result = fPrice.toFixed(18);
+	let arr = price_result.split('.');
 	let int = arr[0];
 	let frac = arr[1].replace(Number(arr[1]).toFixed(0), '');
 	if (int.length > accuracy)
-		price_in_usd = parseFloat(price_in_usd).toFixed(0);
+		price_result = parseFloat(price_result).toFixed(0);
 	else if (int > 0)
-		price_in_usd = parseFloat(price_in_usd).toFixed(accuracy-int.length);
+		price_result = parseFloat(price_result).toFixed(accuracy-int.length);
 	else if (frac[0] !== '0')
-		price_in_usd = parseFloat(price_in_usd).toFixed(accuracy);
+		price_result = parseFloat(price_result).toFixed(accuracy);
 	else if (frac[1] !== '0')
-		price_in_usd = parseFloat(price_in_usd).toFixed(accuracy+1);
+		price_result = parseFloat(price_result).toFixed(accuracy+1);
 	else 
-		price_in_usd = parseFloat(price_in_usd).toFixed(accuracy+frac.length);
-	return price_in_usd;
+		price_result = parseFloat(price_result).toFixed(accuracy+frac.length);
+	return price_result;
 }
 
 exports.formatPriceToPrecision = formatPriceToPrecision;
